37ea78781892ff2ab15eaf0d03f63bd4f5f011d6
AliOlliek <olliek95@gmail.com>
2022-06-04 23:25:02 +0300

    Functioning Login System

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const username = document.getElementById("username").value;
-const password = document.getElementById("pwd");
+
 const loginbtn = document.getElementById("login");
 
+loginbtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("pwd").value;
+
+    let data = new FormData();
+    data.append("username", username);
+    data.append("password", password);
+    let url = "http://localhost/facebook/login.php"
+    let id = localStorage.getItem("idUsers");
+    url += "?idUsers="+id;
+
+    axios({
+        method: 'POST',
+        url: url,
+        data: data,
+    })
+    .then(function(response){ 
+        console.log(response.data)
+        if(response.data.response == "Logged in!"){
+            console.log("Logged in");
+            var get_id = response.data.user_id;
+            console.log("id",get_id)
+        }else{
+            console.log("user not found");
+        }
+    })
+})    
+
+// for (var key of data.entries()) {
+//     console.log(key[0] + ", " + key[1]); || You can not log formdata, but this method works! :)
+//     }
