7379dea2973de24de279121683bd2b09dc8231eb
AliOlliek <olliek95@gmail.com>
2022-06-05 00:36:58 +0300

    Working on Sign up

---
+++ signup.js
@@ -0,0 +1,33 @@
+const sign_up_btn = document.getElementById("signup");
+
+sign_up_btn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("pwd").value;
+
+    let data = new FormData();
+    data.append("username", username);
+    data.append("password", password);
+
+    let url = "localhost/facebook/signup.php";
+
+    axios({
+        
+        method: 'post',
+        url: url,
+        data: data
+    })
+    .then(function (response){
+        console.log(response);
+        if (response == "success!") {
+        document.body.style.backgroundColor = "lightgreen";
+        } else {
+        console.log("please try again :(");
+        }
+    });
+})  
+
+// for (var key of data.entries()) {
+//       console.log(key[0] + ", " + key[1]); // || You can not log formdata, but this method works! :)
+//       }
